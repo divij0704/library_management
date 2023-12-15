7240653bf60748b09f2cc583b08c6fc9f90d66e8
divij0704 <201B100@juetguna.in>
2023-12-15 14:58:08 +0530

    storing the structure of the data model

---
+++ models/book.go
@@ -0,0 +1,13 @@
+// models/book.go
+package models
+
+import (
+	"go.mongodb.org/mongo-driver/bson/primitive"
+)
+
+type Book struct {
+	ID     primitive.ObjectID `json:"_id,omitempty" bson:"_id,omitempty"`
+	Title  string             `json:"title"`
+	Author string             `json:"author"`
+	ISBN   string             `json:"isbn"`
+}
